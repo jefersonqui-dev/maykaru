8de5633596c4d6f02460a1e265c3e167eb0c8ff7
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-06-14 10:52:40 -0500

    Estructuraciones

--- backend/src/main/java/com/jquiguantar/backend/BackendApplication.java
+++ backend/src/main/java/com/jquiguantar/backend/BackendApplication.java
@@ -1,6 +1,8 @@
 // MP/backend/src/main/java/com/jquiguantar/backend/BackendApplication.java
 package com.jquiguantar.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
